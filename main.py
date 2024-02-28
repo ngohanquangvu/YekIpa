d172a7c7517970bcdba05ebbc32b955352af9133
ngohanquangvu <102971556+ngohanquangvu@users.noreply.github.com>
2024-02-28 16:05:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Optional
 
 from fastapi import FastAPI
-
+from requests import *
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return get("https://icanhazip.com/").text
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
